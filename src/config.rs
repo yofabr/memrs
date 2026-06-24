b3d4e3828a4d25f9783a17b8b2c97393878c63ef
Yoftahe <jofab08@gmail.com>
2026-06-24 13:15:34 +0300

    chore: Auth pass hash

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,13 @@
+use argon2::{password_hash::SaltString, Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
+use color_eyre::Result;
+use rand::rngs::OsRng;
 use std::env;
 
-type int = i32;
+type Int = i32;
 
 #[derive(Debug, Clone, Default)]
 pub struct Config {
-    port: int,
+    port: Int,
     password: String,
     // size:
 }
@@ -20,12 +23,27 @@ impl Config {
             config.port = port.parse::<i32>().unwrap();
         }
         if let Ok(password) = env::var("PASSWORD") {
-            config.password = password;
+            config.password = hash_password(&password).unwrap_or_default();
         }
         config
     }
+}
 
-    pub fn print_config(&self) {
-        println!("{:?}", self);
-    }
+pub fn hash_password(pass: &str) -> Result<String> {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+
+    let password_hash = argon2.hash_password(pass.as_bytes(), &salt).unwrap();
+    Ok(password_hash.to_string())
+}
+
+pub fn verify_password(pass: &str, hashed: &str) -> bool {
+    let parsed_hash = match PasswordHash::new(hashed) {
+        Ok(h) => h,
+        Err(_) => return false,
+    };
+
+    Argon2::default()
+        .verify_password(pass.as_bytes(), &parsed_hash)
+        .is_ok()
 }
