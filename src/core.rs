7314386491a86699728d8d0152afc29a51607bb3
Yoftahe <jofab08@gmail.com>
2026-06-26 13:28:22 +0300

    fix: Compile time errors

--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,11 @@
-use tokio::time::Instant;
-use color_eyre::{Result, eyre::eyre};
 use crate::config::Config;
-use std::{collections::{HashMap, HashSet, VecDeque}, sync::{LazyLock, Mutex}, time::Duration};
+use color_eyre::{eyre::eyre, Result};
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    sync::{LazyLock, Mutex},
+    // time::Duration,
+};
+use tokio::time::Instant;
 
 pub static CONFIG: LazyLock<Mutex<Config>> = LazyLock::new(|| {
     let config = Config::load_config();
@@ -10,8 +14,7 @@ pub static CONFIG: LazyLock<Mutex<Config>> = LazyLock::new(|| {
 
 pub trait BasicOps {
     // Creates or updates a key in a database
-    fn set(&self, key: String, value: CacheEntry) -> Result<()>;
-    
+    fn set(&mut self, key: String, value: CacheEntry) -> Result<()>;
 
     // Returns the value of a key
     fn get(&self, key: String) -> Result<CacheEntry>;
@@ -22,10 +25,10 @@ pub trait KeyOps {
     fn exists(&self, key: String) -> Result<()>;
 
     // Deletes a key from a db
-    fn del(&self, key: String) -> Result<()>;
-    
+    fn del(&mut self, key: String) -> Result<()>;
+
     // Frees up db (be careful when using this in a production code)
-    fn flushall(&self);
+    fn flushall(&mut self) -> Result<()>;
 
     // List all the keys matching the passed pattern
     // fn keys(&self, pattern: String);
@@ -34,12 +37,9 @@ pub trait KeyOps {
     // fn expire(&self, key: String, time: Duration);
 }
 
-
 pub trait ListOps {
-
     // Inserts a new element at the head of the list
     fn lpush(&self, key: String, value: String);
-
 
     // Inserts a new element at the tail of the list
     fn rpush(&self, key: String, value: String);
@@ -47,7 +47,6 @@ pub trait ListOps {
     // returns a range of items
     fn lrange(&self, key: String);
 }
-
 
 pub trait SetOps {
     // Adds an element to a set
@@ -62,38 +61,40 @@ pub enum CacheValue {
     STR(String),
     List(VecDeque<String>),
     SET(HashSet<String>),
-    Map(HashMap<String, String>)
+    Map(HashMap<String, String>),
 }
 
 #[derive(Debug, Clone)]
 pub struct CacheEntry {
     pub item: CacheValue,
-    pub ttl: Option<Instant>
+    pub ttl: Option<Instant>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Store {
-    data: HashMap<String, CacheEntry>
+    data: HashMap<String, CacheEntry>,
 }
 
 impl Store {
     fn new(store: HashMap<String, CacheEntry>) -> Self {
-        Self {
-            data: store
-        }
+        Self { data: store }
     }
 }
 
 impl BasicOps for Store {
-    fn set(&self, key: String, value: CacheEntry) -> Result<()> {
+    fn set(&mut self, key: String, value: CacheEntry) -> Result<()> {
         self.data.insert(key, value);
         Ok(())
     }
 
     fn get(&self, key: String) -> Result<CacheEntry> {
         // This will check for expiry dates on each get in the future.
-        let entry = self.data.get(&key)?;
-        Ok(entry.clone())
+        let entry = self.data.get(&key);
+        if let Some(entry) = entry {
+            Ok(entry.clone())
+        } else {
+            Err(eyre!("No records have been found"))
+        }
     }
 }
 
@@ -106,20 +107,19 @@ impl KeyOps for Store {
         }
     }
 
-    fn del(&self, key: String) -> Result<()>{
-
+    fn del(&mut self, key: String) -> Result<()> {
         // More logics will happen on this one
-        let res = self.data.remove(&key);
+        let _res = self.data.remove(&key);
         Ok(())
     }
 
-    fn flushall(&self) -> Result<()> {
+    fn flushall(&mut self) -> Result<()> {
         self.data.clear();
         Ok(())
     }
 }
 
-pub static STORE: LazyLock<Mutex<Store> = LazyLock::new(|| {
+pub static STORE: LazyLock<Mutex<Store>> = LazyLock::new(|| {
     let store: HashMap<String, CacheEntry> = HashMap::new();
     let store_constructor = Store::new(store);
     Mutex::new(store_constructor)
